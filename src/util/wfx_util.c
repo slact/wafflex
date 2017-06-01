1e328bdbda191cf36c1eba4237878a231846fe75
Leo P <junk@slact.net>
2017-05-31 22:19:46 -0400

    ipc_alert_cachemanager seems useful

--- src/util/wfx_util.c
+++ src/util/wfx_util.c
@@ -29,3 +29,15 @@ ngx_int_t wfx_init_timer(ngx_event_t *ev, void (*cb)(ngx_event_t *), void *pd) {
   ev->log = ngx_cycle->log;
   return NGX_OK;
 }
+
+ngx_int_t ipc_alert_cachemanager(ipc_t *ipc, ngx_str_t *name, ngx_str_t *data) {
+  static ngx_int_t slot = NGX_ERROR;
+  if(slot == NGX_ERROR) {
+    int         slot_count;
+    ngx_int_t  *slots;
+    slots = ipc_get_process_slots(ipc, &slot_count, IPC_NGX_PROCESS_CACHE_MANAGER);
+    assert(slot_count == 1);
+    slot = slots[0];
+  }
+  return ipc_alert_slot(ipc, slot, name, data);
+}
--- src/util/wfx_util.h
+++ src/util/wfx_util.h
@@ -1,2 +1,3 @@
 ngx_int_t wfx_init_timer(ngx_event_t *ev, void (*cb)(ngx_event_t *), void *pd);
 ngx_event_t *wfx_add_oneshot_timer(void (*cb)(void *), void *pd, ngx_msec_t delay);
+ngx_int_t ipc_alert_cachemanager(ipc_t *ipc, ngx_str_t *name, ngx_str_t *data);
